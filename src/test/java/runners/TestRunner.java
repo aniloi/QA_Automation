e8c432c5058baada6b0c27cabfae803b51b58377
Aditya Niloi <aniloi@drivewealth.com>
2024-04-17 15:52:01 -0400

    first commit

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,14 @@
+package runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "src/test/java/stepdefs",
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
+)
+
+public class TestRunner {
+}
